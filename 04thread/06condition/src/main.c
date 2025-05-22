aefc080d209a7cb3599fb5722f047cbbccde08ca
VietHoangUbuntu <whoami742002@gmail.com>
2025-05-22 17:12:23 +0700

    update 04thread/06condition

--- 04thread/06condition/src/main.c
+++ 04thread/06condition/src/main.c
@@ -3,51 +3,51 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void* consumer_func(void*);  
-void* provider_func(void*);
+void* assign_func(void*);  
+void* display_func(void*);
 
 volatile char is_ready = 0;
 
 char* data = NULL;
 
-char consumer_done = 0;
+char display_done = 0;
 
 pthread_mutex_t data_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 pthread_cond_t data_cond = PTHREAD_COND_INITIALIZER;
 
 int main() {
-    pthread_t consumer_tid;
-    pthread_create(&consumer_tid, NULL, consumer_func, NULL);
+    pthread_t display_tid;
+    pthread_create(&display_tid, NULL, display_func, NULL);
 
-    pthread_t provider_tid;
-    pthread_create(&provider_tid, NULL, provider_func, NULL);
+    pthread_t assign_tid;
+    pthread_create(&assign_tid, NULL, assign_func, NULL);
 
-    pthread_join(consumer_tid, NULL);
-    pthread_join(provider_tid, NULL);
+    pthread_join(display_tid, NULL);
+    pthread_join(assign_tid, NULL);
 }
 
 
-// /* WITHOUT CONDITION BEGIN---------------------------------------------------------*/
+/* WITHOUT CONDITION BEGIN---------------------------------------------------------*/
 
-void* consumer_func(void* arg) {
-    printf("[CONSUMER------] trying to lock mutex\n");
+void* display_func(void* arg) {
+    printf("[DISPLAY------] trying to lock mutex\n");
     pthread_mutex_lock(&data_mutex);
-    printf("[CONSUMER------] already locked the mutex, gonna sleep 5s ...\n");
+    printf("[DISPLAY------] already locked the mutex, gonna sleep 5s ...\n");
     sleep(5);;
-    printf("[CONSUMER------] done, final data = %s\n", data);
+    printf("[DISPLAY------] done, final data = %s\n", data);
     pthread_mutex_unlock(&data_mutex);
     pthread_exit(NULL);
 }
 
-void* provider_func(void* arg) {
-    printf("[PROVIDER++++++] trying to lock mutex\n");
+void* assign_func(void* arg) {
+    printf("[ASSIGN++++++] trying to lock mutex\n");
     pthread_mutex_lock(&data_mutex);
-    printf("[PROVIDER++++++] already locked the mutex, gonna sleep 5s ...\n");
+    printf("[ASSIGN++++++] already locked the mutex, gonna sleep 5s ...\n");
     sleep(5);
     data = malloc(100);
-    sprintf(data, "from provider\n");
-    printf("[PROVIDER++++++] done\n");
+    sprintf(data, "from assign\n");
+    printf("[ASSIGN++++++] done\n");
     pthread_mutex_unlock(&data_mutex);
      pthread_exit(NULL);
 }
@@ -56,26 +56,26 @@ void* provider_func(void* arg) {
 
 /* WITH CONDITION BEGIN -----------------------------------------------------------*/
 
-// void* consumer_func(void* arg) {
-//     while(!consumer_done) { 
-//         printf("[CONSUMER------] trying to lock mutex\n");
+// void* display_func(void* arg) {
+//     while(!display_done) { 
+//         printf("[DISPLAY------] trying to lock mutex\n");
 
 //         pthread_mutex_lock(&data_mutex);
-//         printf("[CONSUMER------] already locked the mutex\n");
+//         printf("[DISPLAY------] already locked the mutex\n");
 //         while(!is_ready) {
-//             printf("[CONSUMER------] but data aint ready yet, gonna unlock mutex ...\n");
+//             printf("[DISPLAY------] but data aint ready yet, gonna unlock mutex ...\n");
 //             pthread_cond_wait(&data_cond, &data_mutex);
 //         }
-//         printf("[CONSUMER------] data is readay, gonna sleep 5s ...\n");
+//         printf("[DISPLAY------] data is readay, gonna sleep 5s ...\n");
 //         sleep(5);
 
-//         printf("[CONSUMER------] done, final data = %s\n", data);
+//         printf("[DISPLAY------] done, final data = %s\n", data);
 
-//         consumer_done = 1;
+//         display_done = 1;
 
 //         is_ready = 0;
 
-//         printf("[CONSUMER------] consumer_done = %d\n", consumer_done);
+//         printf("[DISPLAY------] display_done = %d\n", display_done);
 
 //         pthread_mutex_unlock(&data_mutex);
 //         //pthread_exit(NULL);
@@ -84,17 +84,17 @@ void* provider_func(void* arg) {
     
 // }
 
-// void* provider_func(void* arg) {
-//     printf("[PROVIDER++++++] trying to lock mutex\n");
+// void* assign_func(void* arg) {
+//     printf("[ASSIGN++++++] trying to lock mutex\n");
 
 //     pthread_mutex_lock(&data_mutex);
-//     printf("[PROVIDER++++++] already locked the mutex, gonna sleep 5s ...\n");
+//     printf("[ASSIGN++++++] already locked the mutex, gonna sleep 5s ...\n");
 //     sleep(5);
 
 //     data = malloc(100);
-//     sprintf(data, "from provider\n");
+//     sprintf(data, "from assign\n");
 
-//     printf("[PROVIDER++++++] done\n");
+//     printf("[ASSIGN++++++] done\n");
 
 //     is_ready = 1;
 
